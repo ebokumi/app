e722b93169f63feb3f2517bd0b658b24acaddb8e
Emmanuel Solomo <solomo@Emmanuels-iMac.home>
2015-09-19 04:22:45 +0100

    update

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('automationlogicApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
